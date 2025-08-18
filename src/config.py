e320468548269c6cd3b35b0028a2403bbd563049
Nguyễn Mạnh Dũng <dungmanh24082006@gmail.com>
2025-08-19 01:35:33 +0700

    Update config.py
    Signed-off-by: Nguyễn Mạnh Dũng <dungmanh24082006@gmail.com>

--- src/config.py
+++ src/config.py
@@ -1,48 +1,47 @@
 # Configuration settings for the Flask application
 
 import os
+import urllib
 
 class Config:
     """Base configuration."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1']
     TESTING = os.environ.get('TESTING', 'False').lower() in ['true', '1']
-    DATABASE_URI = os.environ.get('DATABASE_URI') or "mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB"
+
+    server = "127.0.0.1,1433"
+    database = "FlaskApiDB"
+    username = "sa"
+    password = "A@123456"
+
+    # URL encode password
+    password_encoded = urllib.parse.quote_plus(password)
+
+    DATABASE_URI = (
+        f"mssql+pyodbc://{username}:{password_encoded}@{server}/{database}"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
+    )
+
     CORS_HEADERS = 'Content-Type'
+
 
 class DevelopmentConfig(Config):
     """Development configuration."""
     DEBUG = True
     DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or 'sqlite:///dev.db'
 
+
 class TestingConfig(Config):
     """Testing configuration."""
     TESTING = True
     DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or 'sqlite:///test.db'
 
+
 class ProductionConfig(Config):
     """Production configuration."""
     DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///prod.db'
-    
-template = {
-    "swagger": "2.0",
-    "info": {
-        "title": "Todo API",
-        "description": "API for managing todos",
-        "version": "1.0.0"
-    },
-    "basePath": "/",
-    "schemes": [
-        "http",
-        "https"
-    ],
-    "consumes": [
-        "application/json"
-    ],
-    "produces": [
-        "application/json"
-    ]
-}
+
+
 class SwaggerConfig:
     """Swagger configuration."""
     template = {
@@ -53,16 +52,9 @@ class SwaggerConfig:
             "version": "1.0.0"
         },
         "basePath": "/",
-        "schemes": [
-            "http",
-            "https"
-        ],
-        "consumes": [
-            "application/json"
-        ],
-        "produces": [
-            "application/json"
-        ]
+        "schemes": ["http", "https"],
+        "consumes": ["application/json"],
+        "produces": ["application/json"]
     }
 
     swagger_config = {
